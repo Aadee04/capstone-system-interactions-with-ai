70dd446181203e3360f057c37f66608ef1984e5a
Aadee Sawarkar <aadee2004@gmail.com>
2025-10-21 18:42:20 +0530

    import error in discover apps

--- app/agents/discover_app.py
+++ app/agents/discover_app.py
@@ -6,7 +6,7 @@ def discover_tools():
     for file in os.listdir("app/tools"):
         if file.endswith(".py") and file not in ["__init__.py"]:
             name = file[:-3]
-            module = importlib.import_module(f"tools.{name}")
+            module = importlib.import_module(f"app.tools.{name}")
             for attr_name in dir(module):
                 attr = getattr(module, attr_name)
 
@@ -23,7 +23,7 @@ def discover_tools_descriptions():
     for file in os.listdir("app/tools"):
         if file.endswith(".py") and file != "__init__.py":
             name = file[:-3]
-            module = importlib.import_module(f"tools.{name}")
+            module = importlib.import_module(f"app.tools.{name}")
             for attr_name in dir(module):
                 attr = getattr(module, attr_name)
                 if callable(attr) and hasattr(attr, "name") and hasattr(attr, "description"):
